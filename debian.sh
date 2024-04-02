cc820c9ce83195342a70d82aa0fa70d16022f54e
Patrick Meenan <pmeenan@chromium.org>
2024-04-02 10:39:20 -0400

    Fixed pip install

--- debian.sh
+++ debian.sh
@@ -108,8 +108,8 @@ sudo npm update -g
 #**************************************************************************************************
 # Python Modules
 #**************************************************************************************************
-sudo pip3 install --upgrade --break-system-packages pip
-until sudo pip3 install --break-system-packages dnspython monotonic pillow psutil requests tornado wsaccel \
+sudo pip3 install --upgrade pip
+until sudo pip3 install dnspython monotonic pillow psutil requests tornado wsaccel \
         brotli fonttools selenium future usbmuxwrapper \
         google-api-core google-cloud-pubsub google-cloud-storage pytz tzlocal greenstalk
 do
